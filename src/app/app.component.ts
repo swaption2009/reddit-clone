c9bfc1a6ab1c13650c4c5a5a147182a48f2b0961
swaption2009 <swaption2009@gmail.com>
2016-10-20 01:20:08 -0700

    2nd component
    - create second component with new class and decorator
    - export this class to AppModule
    - import, register and declare in @NgModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-sidebar',
+  template: `
+    <div id="sidebar">
+      Sidebar will go here
+    </div>
+  `
+})
+export class SidebarComponent {
+
+}
+
+@Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `
+    <h1>Hello, {{ title }}</h1>
+    <app-sidebar></app-sidebar>
+    <div id="container">
+      <div id="content">
+        <div>
+          Content will go here
+        </div>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Ron!';
 }
