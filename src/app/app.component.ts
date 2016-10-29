c4aa45f44a9c48aed4a85d44d2c705c6cf5df00a
swaption2009 <swaption2009@gmail.com>
2016-10-28 23:25:24 -0700

    install and add semantic-ui-css and jQuery dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,9 +22,33 @@ export class SidebarComponent {
 @Component({
   selector: 'app-article',
   template: `
-    <div id="article">
-      <h2>{{ article.title }}</h2>
-      <p>{{ article.description }}</p>
+    <div class="image">
+      <img src="https://placekitten.com/g/400/300" />
+    </div>
+    <div class="content">
+      <div class="header">
+        {{ article.title }}
+      </div>
+      <div class="meta">
+        Voting and votes will go here
+      </div>
+      <div class="meta date">
+        Today
+      </div>
+      <div class="meta description">
+        <p>
+          {{ article.description }}
+        </p>
+      </div>
+      <div class="extra">
+        <a 
+          href="#"
+          target="_blank"
+          class="ui right floated button primary">
+          Read more
+          <i class="right chevron icon"></i>
+         </a>
+      </div>
     </div>
   `
 })
@@ -35,14 +59,14 @@ export class ArticleComponent {
 
 @Component({
   selector: 'app-root',
-  templateUrl: `
-    <h1>Hello, {{ title }}</h1>
-    <app-sidebar></app-sidebar>
-    <div id="container">
-      <div id="content">
+  template: `
+    <div class="ui container">
+      <app-sidebar></app-sidebar>
+      <div class="ui divided items">
         <app-article 
           *ngFor="let article of articles"
-          [article]='article'></app-article>
+          [article]='article'
+          class="item"></app-article>
       </div>
     </div>
   `,
