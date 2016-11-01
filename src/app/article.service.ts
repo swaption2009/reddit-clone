8ccb5662650c57211c242bf242235c770d699cce
swaption2009 <swaption2009@gmail.com>
2016-10-31 21:53:10 -0700

    add 2 seconds delay in Promise resolve

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -7,23 +7,27 @@ export class ArticleService {
   constructor() { }
 
   public getArticles(): Promise<Article[]> {
-    return Promise.resolve([
-      new Article(
-        'The Angular 2 screencast',
-        'The easiest way to learn Angular 2',
-        10
-      ),
-      new Article(
-        'Fullstack React',
-        'Let\'s learn React',
-        8
-      ),
-      new Article(
-        'Vue is new',
-        'Vue 2.0 is lightweight',
-        15
-      )
-    ]);
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          new Article(
+            'The Angular 2 screencast',
+            'The easiest way to learn Angular 2',
+            10
+          ),
+          new Article(
+            'Fullstack React',
+            'Let\'s learn React',
+            8
+          ),
+          new Article(
+            'Vue is new',
+            'Vue 2.0 is lightweight',
+            15
+          )
+        ]);
+      }, 2000);
+    });
   }
 
 }
