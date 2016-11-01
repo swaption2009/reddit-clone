1037da2ad417f7ae8ebb8e7f69b353f9e7707344
swaption2009 <swaption2009@gmail.com>
2016-10-31 21:41:29 -0700

    refactor hardcoded articles to getArticle method in articleService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,6 @@ export class AppComponent {
   constructor(
     private articleService: ArticleService
   ) {
-    this.articles = [
-      new Article(
-      'The Angular 2 screencast',
-      'The easiest way to learn Angular 2',
-        10
-      ),
-      new Article(
-      'Fullstack React',
-      'Let\'s learn React',
-        8
-      ),
-      new Article(
-      'Vue is new',
-      'Vue 2.0 is lightweight',
-        15
-      )];
+    this.articles = articleService.getArticles();
   }
 }
--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Article }    from "./article";
 
 @Injectable()
 export class ArticleService {
 
   constructor() { }
+
+  public getArticles(): Article[] {
+    return [
+      new Article(
+        'The Angular 2 screencast',
+        'The easiest way to learn Angular 2',
+        10
+      ),
+      new Article(
+        'Fullstack React',
+        'Let\'s learn React',
+        8
+      ),
+      new Article(
+        'Vue is new',
+        'Vue 2.0 is lightweight',
+        15
+      )];
+  }
 
 }
