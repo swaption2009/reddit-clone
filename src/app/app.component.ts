790f8238d73ae5b9610f1437e2e4e7afe4b9a00c
swaption2009 <swaption2009@gmail.com>
2016-10-31 22:51:04 -0700

    refactor articleService
    move from app-component mounting to article-list component (when
    articles array is called)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { Article }          from './article';
-import { ArticleService }   from "./article.service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  articles: Article[];
-
-  constructor(
-    private articleService: ArticleService
-  ) {
-      articleService.getArticles()
-        .then(articles => this.articles = articles);
-  }
-}
+export class AppComponent { }
