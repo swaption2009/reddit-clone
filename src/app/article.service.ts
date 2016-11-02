a689657af0aaa748fb8dd9026a868ed14bd32d2e
swaption2009 <swaption2009@gmail.com>
2016-11-01 17:06:28 -0700

    add interface to render json from object create
    refactor from constructor method

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -26,14 +26,9 @@ export class ArticleService {
       .then(resp => resp.json())
       .then(json => json.articles)
       .then(articles => {
-        const list = articles
-          .map(article => new Article(
-            article.title,
-            article.description,
-            article.urlToImage
-          ))
-        console.log('json ->', list);
-        return list;
+        console.log('json ->', articles);
+        return articles
+          .map(article => Article.fromJSON(article));
       })
       .catch(err => {
         console.log('We got an error', err);
