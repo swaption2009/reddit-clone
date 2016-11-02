5fdc5027336885a88d9e4fac3663ce091b9757f5
swaption2009 <swaption2009@gmail.com>
2016-11-01 19:11:56 -0700

    replace Promise with Observable

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,36 +1,47 @@
-import { Injectable }             from '@angular/core';
-import { Article }                from './article';
-import { environment }            from '../environments/environment';
-import { Http, URLSearchParams }  from '@angular/http';
+import { Injectable }                   from '@angular/core';
+import { Article }                      from './article';
+import { environment }                  from '../environments/environment';
+import { Http, URLSearchParams }        from '@angular/http';
+import { BehaviorSubject, Observable}   from 'rxjs';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ArticleService {
+  private _articles: BehaviorSubject<Article[]> =
+    new BehaviorSubject<Article[]>([]);
+
+  public articles: Observable<Article[]> = this._articles.asObservable();
 
   constructor(
     private http: Http
   ) { }
 
-  public getArticles(): Promise<Article[]> {
+  public getArticles(): void {
+    // make http request -> observable
+    // convert response into article class
+    // update our subject
+    this._makeHttpRequest('/v1/articles', 'reddit-r-all')
+      .map(json => json.articles)
+      .subscribe(articlesJSON => {
+        const articles = articlesJSON
+          .map(articlesJSON => Article.fromJSON(articlesJSON));
+        this._articles.next(articles);
+      });
+  }
+
+  private _makeHttpRequest(
+    path: string,
+    sourceKey: string
+  ): Observable<any> {
     let params = new URLSearchParams();
     params.set('apiKey', environment.newsApiKey);
-    params.set('source', 'reddit-r-all');
+    params.set('source', sourceKey);
 
     return this.http
-      .get(`${environment.baseUrl}/v1/articles`, {
+      .get(`${environment.baseUrl}${path}`, {
         search: params
-      })
-      .toPromise()
-      .then(resp => resp.json())
-      .then(json => json.articles)
-      .then(articles => {
-        console.log('json ->', articles);
-        return articles
-          .map(article => Article.fromJSON(article));
-      })
-      .catch(err => {
-        console.log('We got an error', err);
-      });
+      }).map(resp => resp.json());
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
 
   baseUrl: 'https://newsapi.org',
   newsApiKey: '793984372a2b4aad9dddc711b810b1cf'
